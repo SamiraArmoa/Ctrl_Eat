71c8d4cf9e3e71f6678129c690c29a0aec9e8426
aroa-im <aroa.i.m@opendeusto.es>
2025-04-01 19:01:52 +0200

    guardar ingredientes en la bbdd

--- Ctrl_Eat/utils/csv_utils.h
+++ Ctrl_Eat/utils/csv_utils.h
@@ -1,6 +1,8 @@
 #ifndef CSV_UTILS_H
 #define CSV_UTILS_H
 
-int cargar_csvs();
+int guardar_ingredientes(int id, char *nombre);
+int guardar_productos(int id, char *nombre, char *ingredientes, char* tipo, float precio );
+int guardar_restaurantes(char *id, char *nombre, char* ciudad);
 
 #endif
