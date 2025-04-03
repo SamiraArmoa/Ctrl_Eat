50e76628a02003247bd30f5da508673955f0c49e
aroa-im <aroa.i.m@opendeusto.es>
2025-04-03 17:46:34 +0200

    Fix csv_utils

--- Ctrl_Eat/utils/csv_utils.h
+++ Ctrl_Eat/utils/csv_utils.h
@@ -1,8 +1,10 @@
 #ifndef CSV_UTILS_H
 #define CSV_UTILS_H
 
+int cargar_csvs();
 int guardar_ingredientes(int id, char *nombre);
-int guardar_productos(int id, char *nombre, char *ingredientes, char* tipo, float precio );
+int guardar_productos(int id, char *nombre, char* tipo, float precio);
 int guardar_restaurantes(char *id, char *nombre, char* ciudad);
+int guardar_productoIngrediente(int id_pr, int id_in);
 
 #endif
