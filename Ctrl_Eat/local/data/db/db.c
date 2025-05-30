018fc0fef813884f17a7037e3f35d33e6a853641
aroa-im <aroa.i.m@opendeusto.es>
2025-05-30 12:30:28 +0200

    anadir metodos guardar a db
    quitar los metodos de csv_utils y añadirlos a db

--- Ctrl_Eat/local/data/db/db.c
+++ Ctrl_Eat/local/data/db/db.c
@@ -208,6 +208,69 @@ int updateEmpleados(int id_em, char *nombre, char *cargo, char *cuentaBancaria) 
 	return 0;
 }
 
+int guardar_productoIngrediente(int id_pr, int id_in) {
+	sqlite3 *db;
+	sqlite3_stmt *stmt;
+	int db_found = sqlite3_open(DB_PATH, &db);
+
+	printf("Producto: %i Ingrediente: %i\n", id_pr, id_in);
+
+	if (db_found != SQLITE_OK) {
+		printf("Error al abrir la base de datos: %s\n", sqlite3_errmsg(db));
+		sqlite3_close(db);
+		return 1;
+	}
+
+	char *insert_sql =
+			"INSERT INTO PRODUCTO_INGREDIENTE(id_pr, id_in) VALUES (?, ?)";
+	sqlite3_prepare_v2(db, insert_sql, strlen(insert_sql) + 1, &stmt, NULL);
+	sqlite3_bind_int(stmt, 1, id_pr);
+	sqlite3_bind_int(stmt, 2, id_in);
+
+	int result = sqlite3_step(stmt);
+	sqlite3_finalize(stmt);
+
+	if (result != SQLITE_DONE) {
+		printf("Insert error: %s\n", sqlite3_errmsg(db));
+	} else {
+		printf("Insert successful\n");
+	}
+	sqlite3_close(db);
+	return 0;
+}
+
+int guardar_productos(int id, char *nombre, char *tipo, float precio) {
+	sqlite3 *db;
+	sqlite3_stmt *stmt;
+	int db_found = sqlite3_open(DB_PATH, &db);
+
+	if (db_found != SQLITE_OK) {
+		printf("Error al abrir la base de datos: %s\n", sqlite3_errmsg(db));
+		sqlite3_close(db);
+		return 1;
+	}
+
+	char *insert_sql =
+			"INSERT INTO Producto(ID_PR, NOMBRE,PRECIO,TIPO) VALUES (?, ?, ?, ?)";
+	sqlite3_prepare_v2(db, insert_sql, strlen(insert_sql) + 1, &stmt, NULL);
+	sqlite3_bind_int(stmt, 1, id);
+	sqlite3_bind_text(stmt, 2, nombre, strlen(nombre), SQLITE_STATIC);
+	sqlite3_bind_double(stmt, 3, precio);
+	sqlite3_bind_text(stmt, 4, tipo, strlen(tipo), SQLITE_STATIC);
+
+	int result = sqlite3_step(stmt);
+	sqlite3_finalize(stmt);
+
+	if (result != SQLITE_DONE) {
+		printf("Insert error: %s\n", sqlite3_errmsg(db));
+	} else {
+		printf("Insert successful\n");
+	}
+
+	sqlite3_close(db);
+	return 0;
+}
+
 int obtenerUltimoIdProductos(int *id) {
 	sqlite3 *db;
 	sqlite3_stmt *stmt;
@@ -246,6 +309,35 @@ int obtenerUltimoIdProductos(int *id) {
 	sqlite3_close(db);
 
 	return SQLITE_OK;
+}
+
+int guardar_ingredientes(int id, char *nombre) {
+	sqlite3 *db;
+	sqlite3_stmt *stmt;
+	int db_found = sqlite3_open(DB_PATH, &db);
+
+	if (db_found != SQLITE_OK) {
+		printf("Error al abrir la base de datos: %s\n", sqlite3_errmsg(db));
+		sqlite3_close(db);
+		return 1;
+	}
+
+	char *insert_sql = "INSERT INTO Ingrediente(ID_IN, NOMBRE) VALUES (?, ?)";
+	sqlite3_prepare_v2(db, insert_sql, strlen(insert_sql) + 1, &stmt, NULL);
+	sqlite3_bind_int(stmt, 1, id);
+	sqlite3_bind_text(stmt, 2, nombre, strlen(nombre), SQLITE_STATIC);
+
+	int result = sqlite3_step(stmt);
+	sqlite3_finalize(stmt);
+
+	if (result != SQLITE_DONE) {
+		printf("Insert error: %s\n", sqlite3_errmsg(db));
+	} else {
+		printf("Insert successful\n");
+	}
+
+	sqlite3_close(db);
+	return 0;
 }
 
 int obtenerIngredientes() {
@@ -745,6 +837,36 @@ int deleteIngredientes(int id_in) {
 	return SQLITE_OK;
 }
 
+int guardar_restaurantes(char *id, char *nombre, char *ciudad) {
+	sqlite3 *db;
+	sqlite3_stmt *stmt;
+	int db_found = sqlite3_open(DB_PATH, &db);
+
+	if (db_found != SQLITE_OK) {
+		printf("Error al abrir la base de datos: %s\n", sqlite3_errmsg(db));
+		sqlite3_close(db);
+		return 1;
+	}
+
+	char *insert_sql =
+			"INSERT INTO Restaurante(ID_RES, NOMBRE, CIUDAD) VALUES (?, ?, ?)";
+	sqlite3_prepare_v2(db, insert_sql, strlen(insert_sql) + 1, &stmt, NULL);
+	sqlite3_bind_text(stmt, 1, id, strlen(id), SQLITE_STATIC);
+	sqlite3_bind_text(stmt, 2, nombre, strlen(nombre), SQLITE_STATIC);
+	sqlite3_bind_text(stmt, 3, ciudad, strlen(ciudad), SQLITE_STATIC);
+
+	int result = sqlite3_step(stmt);
+	sqlite3_finalize(stmt);
+
+	if (result != SQLITE_DONE) {
+		printf("Insert error: %s\n", sqlite3_errmsg(db));
+	} else {
+		printf("Insert successful\n");
+	}
+	sqlite3_close(db);
+	return 0;
+}
+
 int obtenerUltimoIdRestaurante(int *id) {
 	sqlite3 *db;
 	sqlite3_stmt *stmt;
@@ -966,6 +1088,82 @@ int obtenerProductos() {
 	return SQLITE_OK;
 }
 
+int guardarPedidos(int domic,char *fchEntrega, char *fchPedido){
+    sqlite3 *db;
+    sqlite3_stmt *stmt;
+    int rc;
+
+    // Abrir la base de datos
+    rc = sqlite3_open(DB_PATH, &db);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Error al abrir la base de datos: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return rc;
+    }
+
+    // Preparar la consulta SQL para insertar un nuevo pedido
+    char *sql = "INSERT INTO Pedido (FCHA_ENTREGA, FCHA_PEDIDO, DOMICILIO, ID_CL, ID_RES) VALUES (?, ?, ?, ?, ?)";
+    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Error al preparar la consulta: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return rc;
+    }
+
+    // Convertir domic (0 o 1) a cadena
+    const char *domic_str = domic ? "Sí" : "No";
+
+    // Bind de los valores a la consulta
+    sqlite3_bind_text(stmt, 1, fchEntrega, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 2, fchPedido, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 3, domic_str, -1, SQLITE_STATIC);
+    sqlite3_bind_int(stmt, 4, 1); // TODO id_cl valor
+    sqlite3_bind_int(stmt, 5, 1); // TODO id_res calor
+
+    // Ejecutar la consulta
+    rc = sqlite3_step(stmt);
+    if (rc != SQLITE_DONE) {
+        fprintf(stderr, "Error al ejecutar la consulta: %s\n", sqlite3_errmsg(db));
+    } else {
+        printf("Pedido creado exitosamente\n");
+    }
+
+    // Finalizar y cerrar
+    sqlite3_finalize(stmt);
+    sqlite3_close(db);
+
+    return SQLITE_OK;
+}
+
+int guardar_productoPedido(int id_ped, int id_pr){
+	sqlite3 *db;
+	sqlite3_stmt *stmt;
+	int db_found = sqlite3_open(DB_PATH, &db);
+
+	if (db_found != SQLITE_OK) {
+		printf("Error al abrir la base de datos: %s\n", sqlite3_errmsg(db));
+		sqlite3_close(db);
+		return 1;
+	}
+
+	char *insert_sql =
+			"INSERT INTO PRODUCTO_PEDIDO(id_ped, id_producto) VALUES (?, ?)";
+	sqlite3_prepare_v2(db, insert_sql, strlen(insert_sql) + 1, &stmt, NULL);
+	sqlite3_bind_int(stmt, 1, id_ped);
+	sqlite3_bind_int(stmt, 2, id_pr);
+
+	int result = sqlite3_step(stmt);
+	sqlite3_finalize(stmt);
+
+	if (result != SQLITE_DONE) {
+		printf("Insert error: %s\n", sqlite3_errmsg(db));
+	} else {
+		printf("Insert successful\n");
+	}
+	sqlite3_close(db);
+	return 0;
+}
+
 int obtenerUltimoIdPedido(int *id) {
 	sqlite3 *db;
 	sqlite3_stmt *stmt;
@@ -1006,7 +1204,7 @@ int obtenerUltimoIdPedido(int *id) {
 	return SQLITE_OK;
 }
 
-int getrPedidos() {
+int getPedidos() {
     sqlite3 *db;
     sqlite3_stmt *stmt;
     int rc;
